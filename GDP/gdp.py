ccb199b02a6cf975a80543134480d544ac7c80bd
Methode TWIZEYIMANA <tmmethode1@gmail.com>
2023-11-12 18:08:25 +0200

    updating GDP Folder

--- GDP/gdp.py
+++ GDP/gdp.py
@@ -12,38 +12,57 @@ excel_file = 'GDP.xlsx'
 def all():
   # Select the worksheet you want to display
   sheet_name = 'in billion Rwf'
+  sheet_name2= 'Shares at current prices'
 
    # Read the worksheet into a Pandas DataFrame
-  df = pd.read_excel(excel_file, sheet_name)
-
+  df1 = pd.read_excel(excel_file, sheet_name)
+  df2 = pd.read_excel(excel_file, sheet_name2)
+  df2 = df2.apply(lambda x: x * 100)
 
   st.header("Macro-economic aggregates")
+  table1,table2=st.columns(2)
+  with table1:
+    # Create a multiselect widget
+    selected_columns = st.multiselect('Filter: ',df1.columns,default=["YEAR","GROSS DOMESTIC PRODUCT (GDP)","INDUSTRY","SERVICES","TAXES LESS SUBSIDIES ON PRODUCTS"])
 
-  # Create a multiselect widget
-  selected_columns = st.multiselect('Filter: ',df.columns,default=["YEAR","GROSS DOMESTIC PRODUCT (GDP)","INDUSTRY","SERVICES","TAXES LESS SUBSIDIES ON PRODUCTS"])
+    # Filter the DataFrame based on the selected columns
+    df_filtered = df1[selected_columns]
+    # Convert the year column to a Pandas datetime object
+    df_filtered['YEAR'] = pd.to_datetime(df1['YEAR'])
 
-  # Filter the DataFrame based on the selected columns
-  df_filtered = df[selected_columns]
-  # Convert the year column to a Pandas datetime object
-  df_filtered['YEAR'] = pd.to_datetime(df['YEAR'])
+    # Extract the date from the Pandas datetime object
+    df_filtered['YEAR'] = df_filtered['YEAR'].dt.date
+    # Display the filtered DataFrame in Streamlit
+    st.dataframe(df_filtered,use_container_width=True)
+  with table2:
+    # Create a multiselect widget
+    selected_columns2 = st.multiselect('Filter: ',df2.columns,default=["YEAR","GROSS DOMESTIC PRODUCT (GDP)","INDUSTRY","SERVICES","TAXES LESS SUBSIDIES ON PRODUCTS"], key=2)
 
-  # Extract the date from the Pandas datetime object
-  df_filtered['YEAR'] = df_filtered['YEAR'].dt.date
-  # Display the filtered DataFrame in Streamlit
+    # Filter the DataFrame based on the selected columns
+    df2_filtered = df2[selected_columns]
+    # Convert the year column to a Pandas datetime object
+    df2_filtered['YEAR'] = pd.to_datetime(df2['YEAR'])
 
-  st.dataframe(df_filtered)
-  
+    # Extract the date from the Pandas datetime object
+    df2_filtered['YEAR'] = df2_filtered['YEAR'].dt.date
+    # Display the filtered DataFrame in Streamlit
+    st.dataframe(df2_filtered,use_container_width=True)
+  ## Graph
   st.subheader("Graph")
-  column_names = df.columns.tolist()
+  column_names = df1.columns.tolist()
   column_names.remove('YEAR')
 
-
-   # Create a multiselect widget
-  selected_columns = st.multiselect('Filter: ',df.columns,default=["GROSS DOMESTIC PRODUCT (GDP)","INDUSTRY","SERVICES","TAXES LESS SUBSIDIES ON PRODUCTS"])
-
-  fig = px.line(df, x='YEAR', y=selected_columns, title='Gross Domestic product by Kind of Activity at current prices ( in billion Rwf)')
-
   left,right=st.columns(2)
-  left.plotly_chart(fig,use_container_width=True)
-  right.plotly_chart(fig,use_container_width=True)
+   # Create a multiselect widget
+  with left:
+    selected_columns = st.multiselect('Filter: ',df1.columns,default=["GROSS DOMESTIC PRODUCT (GDP)","INDUSTRY","SERVICES","TAXES LESS SUBSIDIES ON PRODUCTS"])
+    
+    fig= px.line(df1, x='YEAR', y=selected_columns, title='Gross Domestic product by Kind of Activity at current prices ( in billion Rwf)')
+    fig.update_layout(yaxis_title="Rwf in billion")
+    left.plotly_chart(fig,use_container_width=True)
+  with right:
+    selected_columns2 = st.multiselect('Filter: ',df2.columns,default=["GROSS DOMESTIC PRODUCT (GDP)","INDUSTRY","SERVICES","TAXES LESS SUBSIDIES ON PRODUCTS"],key=1)
+    fig2 = px.line(df2, x='YEAR', y=selected_columns2, title='Gross Domestic product by Kind of Activity Shares at current prices ( percentages)')
+    fig2.update_layout(yaxis_title="Percentage")
+    right.plotly_chart(fig2,use_container_width=True)
 all()
