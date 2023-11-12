cc746e3ad0f034c3a31158f11463cfd0e3e2e04c
Bikorimana Saveur <bikorimanaxavier@gmail.com>
2023-11-13 01:51:07 +0200

    added expenditure data

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with open('style.css')as f:
 #                          GROSS DOMESTIC PRODUCT AT MACRO ECCONOMIC LEVEL
 # -------------------------------------------------------------------------------------------------------------
 #load excel file
-df1=pd.read_excel('Tutorial Spread.xlsx', sheet_name='Sheet1')
+df1=pd.read_excel('Tutorial Spread.xlsx', sheet_name='macro_economic')
 df1 = df1.rename(columns=lambda x: x.strip())
 
 #Macro Economic Table
@@ -60,33 +60,69 @@ def donut_chart():
  
 
 def barchart_with_line():
+    df2=pd.read_excel('Tutorial Spread.xlsx', sheet_name='Table2A')
+    df2 = df2.rename(columns=lambda x: x.strip())
+
     # Create a dataframe with the data from the image
     data = pd.DataFrame({
-        'Year': df1["Years"][18:],
-        'Agriculture': [x*100 for x in df1['Agriculture'][18:]],
-        'Industry': [x*100 for x in df1['Industry'][18:]],
-        'Services': [x*100 for x in df1['Services'][18:]],
-        'Adjustments': [x*100 for x in df1['Adjustments'][18:]],
+        'Year': df2["Activity description"][18:],
+        'Agriculture': [x*100 for x in df2['AGRICULTURE, FORESTRY & FISHING'][18:]],
+        'Industry': [x*100 for x in df2['INDUSTRY'][18:]],
+        'Services': [x*100 for x in df2['SERVICES'][18:]],
+        'GDP': [x*100 for x in df2['GROSS DOMESTIC PRODUCT (GDP)'][18:]],
     })
 
     # Create the traces for the chart
     trace1 = go.Bar(x=data['Year'], y=data['Agriculture'], name='Agriculture')
     trace2 = go.Bar(x=data['Year'], y=data['Industry'], name='Industry')
     trace3 = go.Bar(x=data['Year'], y=data['Services'], name='Services')
-    trace4 = go.Bar(x=data['Year'], y=data['Adjustments'], name='Adjustments')
+    trace4 = go.Scatter(x=data['Year'], y=data['GDP'], name='GDP')
 
     # Create the layout for the chart
     layout = go.Layout(
-        title='Value Added by Agriculture, Industry and Services',
+        title='Percentage Change in Agriculture, Industry, Services, and GDP',
         xaxis=dict(title='Years'),
-        yaxis=dict(title='Percentage Change', range=[-7.5, 60]),
+        yaxis=dict(title='Percentage Change', range=[-7.5, 18]),
         barmode='group'
     )
 
     # Create the figure and plot it using Streamlit
     fig = go.Figure(data=[trace1, trace2, trace3,trace4], layout=layout)
     st.plotly_chart(fig, use_container_width=True)
- 
+
+def expanditure_on_gdp():
+    df3=pd.read_excel('Tutorial Spread.xlsx', sheet_name='Table4A')
+    df3 = df3.rename(columns=lambda x: x.strip())
+   # Create a dataframe with the data from the image
+    data = pd.DataFrame({
+        'Year': df3["Years"][8:],
+        'Gross capital formation': df3["Gross capital formation"][8:],
+        'Exports G&S': df3["Exports of goods & services"][8:],
+        'Households': df3["Households and NGOs"][8:],
+        'Government':df3["Government"][8:],
+        'Imports G&S': df3["Imports of goods & services"][8:],
+        'GDP': df3["Gross Domestic Product"][8:]
+    })
+
+    # Create the traces for the chart
+    trace1 = go.Bar(x=data['Year'], y=data['Gross capital formation'], name='Gross capital formation', marker=dict(color='blue'))
+    trace2 = go.Bar(x=data['Year'], y=data['Exports G&S'], name='Exports G&S', marker=dict(color='orange'))
+    trace3 = go.Bar(x=data['Year'], y=data['Households'], name='Households', marker=dict(color='green'))
+    trace4 = go.Bar(x=data['Year'], y=data['Government'], name='Government', marker=dict(color='red'))
+    trace5 = go.Bar(x=data['Year'], y=data['Imports G&S'], name='Imports G&S', marker=dict(color='yellow'))
+    trace6 = go.Scatter(x=data['Year'], y=data['GDP'], name='GDP', line=dict(color='black'))
+
+    # Create the layout for the chart
+    layout = go.Layout(
+        title='Proportions of GDP and Percentage Change in GDP',
+        xaxis=dict(title='Year'),
+        yaxis=dict(title='Billions of Francs', range=[-5000, 19000]),
+        barmode='stack'
+    )
+
+    # Create the figure and plot it using Plotly
+    fig = go.Figure(data=[trace1, trace2, trace3, trace4, trace5, trace6], layout=layout)
+    st.plotly_chart(fig, use_container_width=True)
 
 def MacroEconomicHome():
     st.title(":house: Macro Economic Aggregate")
@@ -106,6 +142,9 @@ def MacroEconomicHome():
         donut_chart()
     with col2:
         barchart_with_line()
+
+    st.subheader("Expenditure on GDP (Billion Frw)")
+    expanditure_on_gdp()
 #                                       CONSUMER PRICE INDEX
 # -------------------------------------------------------------------------------------------------------------
 
